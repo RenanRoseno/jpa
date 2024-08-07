cd95131a064325a8cbec207c22af88b7e7ad3f52
Renan Roseno <rosenorenan@gmail.com>
2024-08-07 15:09:28 -0300

    feat:add repositories to all entities

---
+++ src/main/java/com/bookstore/jpa/repositories/BookRepository.java
@@ -0,0 +1,16 @@
+package com.bookstore.jpa.repositories;
+
+import com.bookstore.jpa.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BookRepository extends JpaRepository<Book, UUID> {
+    Book findBookByTitle(String title);
+
+    @Query(value = "SELECT * FROM tb_books WHERE publisher_id = :id", nativeQuery = true)
+    List<Book> findBookByPublisherId(@Param("id") UUID id);
+}
